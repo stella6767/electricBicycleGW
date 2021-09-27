16b354cd4cb7e90739ba7b7e0f740c58f568afba
강민규 <alsrb9434@gmail.com>
2021-09-27 14:01:35 +0900

    test 주석처리2

--- src/test/java/net/linalabs/station/handler/ExceptionTest.java
+++ src/test/java/net/linalabs/station/handler/ExceptionTest.java
@@ -10,28 +10,28 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(SocketService.class)
-public class ExceptionTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    public void 테스트_실패_케이스() throws Exception {
-
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(SocketService.class)
+//public class ExceptionTest {
 //
-//        mockMvc.perform(post("/exception/test")
-//                        .content(content)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$.message").value("Null Point Exception!!"))
-//                .andDo(print());
-
-    }
-
-
-}
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Test
+//    public void 테스트_실패_케이스() throws Exception {
+//
+////
+////        mockMvc.perform(post("/exception/test")
+////                        .content(content)
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .accept(MediaType.APPLICATION_JSON))
+////                .andExpect(jsonPath("$.message").value("Null Point Exception!!"))
+////                .andDo(print());
+//
+//    }
+//
+//
+//}
