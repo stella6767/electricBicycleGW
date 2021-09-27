62ac9484013dda844f597ec11ef7fc89127a0cf8
강민규 <alsrb9434@gmail.com>
2021-09-27 14:00:03 +0900

    test 주석처리

--- src/test/java/net/linalabs/station/utills/CommonTest.java
+++ src/test/java/net/linalabs/station/utills/CommonTest.java
@@ -8,24 +8,24 @@ import java.io.IOException;
 @Slf4j
 public class CommonTest {
 
-    @Test
-    public void timeOutTest() throws InterruptedException, IOException {
-
-        int i = 1;
-        //timeOut을 설정해야 될 필요가 있슴.
-        String test = null;
-
-        while (test == null){
-
-            Thread.sleep(10);
-            log.info("ㅋ");
-            i++;
-
-            if(3000 < i*10){
-                throw new IOException();
-            }
-        }
-    }
+//    @Test
+//    public void timeOutTest() throws InterruptedException, IOException {
+//
+//        int i = 1;
+//        //timeOut을 설정해야 될 필요가 있슴.
+//        String test = null;
+//
+//        while (test == null){
+//
+//            Thread.sleep(10);
+//            log.info("ㅋ");
+//            i++;
+//
+//            if(3000 < i*10){
+//                throw new IOException();
+//            }
+//        }
+//    }
 
 
 }
